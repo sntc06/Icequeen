076e75074182923e169572f0d71a4c7b5342789d
Edward Tsai <taya86334@gmail.com>
2012-07-14 14:03:50 +0800

    Updated theme settings and button styles.
    Change-Id: Ib51123e8d4d0ed6deeae60b147228f5d29f46838

--- gen/pl/polidea/coverflow/R.java
+++ gen/pl/polidea/coverflow/R.java
@@ -56,6 +56,14 @@ containing a value of this type.
          */
         public static final int withReflection=0x7f010002;
     }
+    public static final class color {
+        public static final int background_color=0x7f060005;
+        public static final int black=0x7f060004;
+        public static final int green=0x7f060002;
+        public static final int red=0x7f060000;
+        public static final int white=0x7f060003;
+        public static final int yellow=0x7f060001;
+    }
     public static final class drawable {
         public static final int button_style=0x7f020000;
         public static final int ic_launcher=0x7f020001;
@@ -68,29 +76,29 @@ containing a value of this type.
         public static final int image06=0x7f020008;
     }
     public static final class id {
-        public static final int btnConfirmChapter=0x7f060006;
-        public static final int btnLearn=0x7f060014;
-        public static final int btnNext=0x7f06000f;
-        public static final int btnPrevious=0x7f06000d;
-        public static final int btnReview=0x7f060015;
-        public static final int btnSelectAll=0x7f060004;
-        public static final int btnSelectFinal=0x7f060003;
-        public static final int btnSelectMid=0x7f060002;
-        public static final int btnTest=0x7f060016;
-        public static final int button1=0x7f06000b;
-        public static final int chapterCB=0x7f060001;
-        public static final int chapterSelectionList=0x7f060005;
-        public static final int chapterTitleTV=0x7f060000;
-        public static final int coverflow=0x7f060011;
-        public static final int coverflowReflection=0x7f060012;
-        public static final int ratingBar=0x7f06000e;
-        public static final int tvAppName=0x7f060010;
-        public static final int tvClass=0x7f060009;
-        public static final int tvCount=0x7f06000c;
-        public static final int tvMeaning=0x7f060008;
-        public static final int tvSelectedChapter=0x7f060013;
-        public static final int tvSentence=0x7f06000a;
-        public static final int tvWord=0x7f060007;
+        public static final int btnConfirmChapter=0x7f070006;
+        public static final int btnLearn=0x7f070014;
+        public static final int btnNext=0x7f07000f;
+        public static final int btnPrevious=0x7f07000d;
+        public static final int btnReview=0x7f070015;
+        public static final int btnSelectAll=0x7f070004;
+        public static final int btnSelectFinal=0x7f070003;
+        public static final int btnSelectMid=0x7f070002;
+        public static final int btnTest=0x7f070016;
+        public static final int button1=0x7f07000b;
+        public static final int chapterCB=0x7f070001;
+        public static final int chapterSelectionList=0x7f070005;
+        public static final int chapterTitleTV=0x7f070000;
+        public static final int coverflow=0x7f070011;
+        public static final int coverflowReflection=0x7f070012;
+        public static final int ratingBar=0x7f07000e;
+        public static final int tvAppName=0x7f070010;
+        public static final int tvClass=0x7f070009;
+        public static final int tvCount=0x7f07000c;
+        public static final int tvMeaning=0x7f070008;
+        public static final int tvSelectedChapter=0x7f070013;
+        public static final int tvSentence=0x7f07000a;
+        public static final int tvWord=0x7f070007;
     }
     public static final class layout {
         public static final int chapter_list=0x7f030000;
