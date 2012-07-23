3b6f3db8ff2701806114a5365bb60a60025eae39
Edward Tsai <taya86334@gmail.com>
2012-07-23 23:29:21 +0800

    Added PreRreview, Review, PostReview, PreTest, Test, PostTest activities.
    Signed-off-by: Edward Tsai <taya86334@gmail.com>

---
+++ src/com/mis/icequeen/PreReview.java
@@ -0,0 +1,14 @@
+package com.mis.icequeen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreReview extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pre_review);
+    }
+
+}
---
+++ src/com/mis/icequeen/PreTest.java
@@ -0,0 +1,14 @@
+package com.mis.icequeen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PreTest extends Activity {
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pre_test);
+    }
+
+}
